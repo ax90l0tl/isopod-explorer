ad927604f018bc17669f6570b10c9cbd77983e85
ax90l0tl <ax90l0tl@gmail.com>
2024-10-10 07:50:19 -0700

    reorganize and thruster macro

--- ros2_ws/src/rov_sim/launch/demo.launch.py
+++ ros2_ws/src/rov_sim/launch/demo.launch.py
@@ -13,7 +13,7 @@ import xacro
 def generate_launch_description():
 
     # Check if we're told to use sim time
-    use_sim_time = LaunchConfiguration('sim_time')
+    use_sim_time = LaunchConfiguration('use_sim_time')
 
     # Process the URDF file
     pkg_path = os.path.join(get_package_share_directory('rov_sim'))
@@ -21,7 +21,7 @@ def generate_launch_description():
     robot_description_config = xacro.process_file(xacro_file)
     
     # Create a robot_state_publisher node
-    params = {'robot_description': robot_description_config.toxml(), 'sim_time': use_sim_time}
+    params = {'robot_description': robot_description_config.toxml(), 'use_sim_time': use_sim_time}
     node_robot_state_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
@@ -33,7 +33,7 @@ def generate_launch_description():
     # Launch!
     return LaunchDescription([
         DeclareLaunchArgument(
-            'sim_time',
+            'use_sim_time',
             default_value='false',
             description='Use sim time if true'),
 
---
+++ ros2_ws/src/rov_sim/launch/sim.launch.py
@@ -0,0 +1,42 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+from launch_ros.actions import Node
+
+
+
+def generate_launch_description():
+    package_name='rov_sim'
+
+    robot_desc = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory(package_name),'launch','demo.launch.py'
+                )]), launch_arguments={'use_sim_time': 'true'}.items()
+    )
+
+    # Include the Gazebo launch file, provided by the gazebo_ros package
+    gazebo = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py')]),
+             )
+
+    # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
+    spawn_entity = Node(package='ros_gz_sim', executable='create',
+                        arguments=['-topic', 'robot_description',
+                                   '-entity', 'rov'],
+                        output='screen')
+
+
+
+    # Launch them all!
+    return LaunchDescription([
+        robot_desc,
+        gazebo,
+        spawn_entity,
+    ])

93e71c19648edda8a40975e109172674ab38c710
Alex Xu <ax90l0tl@gmail.com>
2024-12-15 19:13:34 -0800

    add ros_gz_bridge

--- ros2_ws/src/rov_sim/launch/demo.launch.py
+++ ros2_ws/src/rov_sim/launch/demo.launch.py
@@ -48,5 +48,5 @@ def generate_launch_description():
             description='Use sim time if true'),
 
         node_robot_state_publisher,
-        joint_robot_state_publisher
+        # joint_robot_state_publisher
     ])
